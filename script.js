ea923b79c10fd48f5d0c451f6b2625a7c6c7dd76
joaoehrich <joaoehrich@gmail.com>
2026-03-19 10:59:54 -0300

    adicionando funcionalidades ao prrenchimento dos campos

--- script.js
+++ script.js
@@ -15,12 +15,49 @@
     }
 });
 
-radioTurno.forEach(radio => {
-    radio.addEventListener('change', function () {
-        if (this.value === 'tarde') {
-            blocoTelefone.style.display = 'block';
-        } else {
-            blocoTelefone.style.display = 'none';
-        }
+    radioTurno.forEach(radio => {
+        radio.addEventListener('change', function () {
+            if (this.value === 'tarde') {
+                blocoTelefone.style.display = 'block';
+            } else {
+                blocoTelefone.style.display = 'none';
+            }
     });
 });
+
+    document.getElementById('nascimento').addEventListener('change', function(){
+        const nascimento = new Date(this.value);
+        const hoje = new Date();
+        if(!this.value){
+            document.getElementById('idade').value='';
+            alert('Digite uma data válida!');
+        }
+
+        let anos = hoje.getFullYear() - nascimento.getFullYear();
+        document.getElementById('idade').value = `${anos} anos`;
+});
+    const campoCep = document.getElementById('cep');
+    campoCep.addEventListener('input', function(){
+        let valor = this.value.replace(/\D/g,'');
+
+        if(valor.length > 5){
+
+            valor = valor.slice(0,2)+'.'+ valor.slice(2,5) +'-'+valor.slice(5,8);
+           
+        }
+
+        this.value = valor;
+      })
+      
+      document.getElementById('cpf').addEventListener('input', function(e){
+        let valor = e.target.value.replace(/\D/g,'');
+        
+        if(valor.length <= 11){
+            valor = valor.replace(/(\d{3})(\d)/, '$1.$2').replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        }
+
+      });
+    
+
+
